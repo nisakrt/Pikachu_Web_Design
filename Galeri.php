5a81900f7f145e39d734a52b5d7d88283986dc70
nisakrt <57156779+nisakrt@users.noreply.github.com>
2021-06-13 16:34:39 +0300

    Add files via upload

---
+++ Galeri.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> 
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
+<script type="text/javascript" src="js/jquery-1.5.1.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans" type="text/css">
+<link rel="stylesheet" href="css/main.css">
+
+  <title>GALERİ</title>
+</head>
+<body>
+<?php include 'header.php';?>
+<div class="ana-bolum" >
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/1.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/2.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/3.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/4.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="ayirici"></div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/5.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/6.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/7.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/8.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="ayirici"></div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/1.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/9.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/10.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/11.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="ayirici"></div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/12.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/14.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/15.jpg" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+  <div class="kutu">
+    <a href="#" class="fotograf"><img src="img/16.png" width="190" height="152" alt="Fotoğraf Başlığı" /></a>
+    <a href="#" class="baslik">PİKACHU TURKEY</a>
+  </div>
+</div>
+
+<footer id="altfooter">
+ <p>Copyright 2021 | Tüm haklar saklıdır.</p>
+</footer>
+
+</body>
+
+</html>
